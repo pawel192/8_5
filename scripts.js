caa7b91557b19aa0ba323cb965fc99c836681cca
Pawel Krajewski <krajewski_pawel92@o2.pl>
2018-01-31 20:17:29 +0100

    finished

--- scripts.js
+++ scripts.js
@@ -1,4 +1,5 @@
-var a:prompt ('podaj długosć a');
-var h:prompt (podaj wysokość h);
-var trinagleArea: a*h/2;
+var a = prompt ('podaj długosć a');
+var h = prompt ('podaj wysokość h');
+var triangleArea = a*h/2;
+alert('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
 console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
